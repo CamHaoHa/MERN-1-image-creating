88ba4f4efe9475da4e1753a2ac541a4a9033e62d
CamHaoHa <camhaovn@gmail.com>
2023-01-26 20:22:52 +1100

    modified api url

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,7 +21,7 @@ const CreatePost = () => {
     if(form.prompt) {
       try{
           setGeneratingImg(true)
-          const response = await fetch('http://localhost:8080/api/v1/dalle' ,{
+          const response = await fetch('https://mern-1-image-creating.onrender.com/api/v1/dalle' ,{
             method: 'POST',
             headers :{
               'Content-Type': 'application/json',
@@ -49,7 +49,7 @@ const CreatePost = () => {
 
       try {
         const response = await fetch(
-            'http://localhost:8080/api/v1/post', 
+            'https://mern-1-image-creating.onrender.com/api/v1/post', 
             {
               method: 'POST', 
               headers: {
